5e8e1a3a0a1970a088574e2bff6129a120d2d2f8
Paulina Hudzik <paulinanunab@gmail.com>
2022-11-06 22:01:43 +0100

    ver 1.4

--- main.cpp
+++ main.cpp
@@ -2,32 +2,69 @@
 
 using namespace std;
 
+int opcja=0;
+
 int main()
 {
-    int a=0,b=0,x=0;
 
-    cout<<"Podaj wartosc A ";
-    cin>>a;
-    cout<<"Podaj wartosc B ";
-    cin>>b;
+    cout<<"Witaj, wybierz rownanie."<<endl;
+    cout<<"1. Ax+B=0"<<endl;
+    cout<<"2. Ax+By+C=0"<<endl;
+    cout<<"3. Ax^2+By+C=0"<<endl;
+    do{
+        cout<<"Opcja nr.";
+        cin>>opcja;
+    }
+    while(opcja!=1&&opcja!=2&&opcja!=3);
+    cout<<endl;
 
-    if (a==0)
+    if(opcja==1)
     {
-        if(b==0)
+    int a=0,b=0,x=0;
+                cout<<"Podaj wartosc A ";
+                cin>>a;
+                cout<<"Podaj wartosc B ";
+                cin>>b;
+
+        if (a==0)
         {
-            cout<<"F.stala";
-        }
-        else
-        {
-            cout<<"brak m.zerowych";
-        }
-        }
-        else
-        {
-            x=(-b)/a;
+            if(b==0)
+            {
+                cout<<"F.stala";
+            }
+            else
+            {
+                cout<<"brak m.zerowych";
+            }
+            }
+            else
+            {
+                x=(-b)/a;
             cout<<"Miejscem zerowym jest "<<x;
+            }
+    }
+
+
+    if(opcja==2)
+    {
+        int a=0,b=0,c=0,x=0;
+        cout<<"Podaj wartosc A ";
+        cin>>a;
+        cout<<"Podaj wartosc B ";
+        cin>>b;
+        if (b!=0)
+        {
+        cout<<"Podaj wartosc C ";
+        cin>>c;
+        x=(-c)/a;
+        cout<<"Miejscem zerowym jest "<<x;
         }
-cout<<endl;
+        else
+        {
+        cout<<"B nie moze byc rowne 0!!";
+        }
+    }
 
     return 0;
+
 }
